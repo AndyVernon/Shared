8b01917303fe6a26de9d7abb4bf77c365c9dbfd1
andyvernon <andy_vernon@live.co.uk>
2023-07-25 20:33:14 +0100

    Added test API for Shared.User.Example

---
+++ tests/Shared.User.Example.TestAPI/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shared.User.Example.TestAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [Route("IsValidUsername")]
+        public IActionResult IsValidUsername(string username)
+        {
+            return Ok();
+        }
+    }
+}
