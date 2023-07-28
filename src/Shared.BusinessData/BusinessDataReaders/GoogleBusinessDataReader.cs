224c737663271dbdde6cb8c02fbdbc78964d97a7
andyvernon <andy_vernon@live.co.uk>
2023-07-28 20:36:45 +0100

    Renamed solution, ignore users.json file, added business data for getting file from Google Cloud Storage

---
+++ src/Shared.BusinessData/BusinessDataReaders/GoogleBusinessDataReader.cs
@@ -0,0 +1,33 @@
+﻿using Google.Cloud.Storage.V1;
+using Shared.BusinessData.Interfaces;
+
+namespace Shared.BusinessData.BusinessDataReaders
+{
+    internal class GoogleBusinessDataReader : IBusinessDataReader<Google.Apis.Storage.v1.Data.Object>
+    {
+        private readonly GoogleBusinessDataReader _this;
+
+        public GoogleBusinessDataReader()
+        {
+            _this = this;
+        }
+
+        public IEnumerable<Google.Apis.Storage.v1.Data.Object> ListFiles(string containerName)
+        {
+            var storage = StorageClient.Create();
+            var storageObjects = storage.ListObjects(containerName);
+            return storageObjects;
+        }
+
+        public Google.Apis.Storage.v1.Data.Object? GetFile(string containerName, string fileName)
+        {
+            var files = _this.ListFiles(containerName);
+            if (files == null)
+            {
+                return null;
+            }
+
+            return files.FirstOrDefault(x => x.Name == fileName);
+        }
+    }
+}
---
+++ src/Shared.BusinessData/Interfaces/IBusinessDataReader.cs
@@ -0,0 +1,8 @@
+﻿namespace Shared.BusinessData.Interfaces
+{
+    public interface IBusinessDataReader<T>
+    {
+        IEnumerable<T> ListFiles(string containerName);
+        T? GetFile(string containerName, string fileName);
+    }
+}
